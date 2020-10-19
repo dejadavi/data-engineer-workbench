e5717f86c469f94af6c09a8564207dacbd48fe29
Desmond Davis <dejadavi@MacBook-Pro.local>
2020-10-19 16:42:32 -0500

    working mvp

--- build-images.sh
+++ build-images.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+mkdir -p ./files/jupyter
+mkdir -p ./files/spark/data/spark-events
+mkdir -p ./files/spark/data/spark-events
+mkdir -p ./files/spark/data/tmp
+mkdir -p ./files/hive/data
+mkdir -p ./files/hadoop/data
+
 docker build -t spark-base:2.7.7 ./docker/base
 docker build -t spark-master:2.7.7 ./docker/spark-master
 docker build -t spark-worker:2.7.7 ./docker/spark-worker
--- docker/spark-master/start-master.sh
+++ docker/spark-master/start-master.sh
@@ -6,8 +6,6 @@ export SPARK_MASTER_HOST=`hostname`
 
 mkdir -p $SPARK_MASTER_LOG
 
-export SPARK_HOME=/opt/spark
-
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
 cd /opt/spark/bin && /opt/spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
